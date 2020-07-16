ac2fc88dae73fb7a7951555d45421b034ee59c4f
Dharma teja Koneru <koneru@MacBook-Pro.local>
2020-07-15 20:33:43 -0700

    my first commit to git

---
+++ babycoder.git/javascript.js
@@ -0,0 +1,20 @@
+function grow(){
+    document.getElementById("box").style.height = "250px";
+}
+function reset(){
+    document.getElementById("box").style.height = "150px";
+    document.getElementById("box").style.backgroundColor = "orange";
+    document.getElementById("box").style.opacity = "1";
+}
+function blue(){
+    document.getElementById("box").style.backgroundColor = "blue";
+}
+function fade(){
+    document.getElementById("box").style.opacity = "0.5";
+}
+function shrink(){
+    document.getElementById("box").style.height = "25px"; 
+}
+function fadeout(){
+    document.getElementById("box").style.opacity = "1.0";
+}
